0c775564854e327473ae9a5c57a7a8756af3403a
Catuxa.aljo <catuxa.aljo@gmail.com>
2021-10-03 20:47:32 +0200

    Fix: photo album experience

--- web/src/components/album/Album.js
+++ web/src/components/album/Album.js
@@ -5,18 +5,18 @@ function Album({albums, title}) {
     return(
         <div className="photo">
             <div className="photoalbum">
-                {albums.length > 0 
+            {albums.length > 0 
                     ? 
                     <div>                        
                         <NavLink exact to={`/my-travels/album/${albums[0].id}`}> 
-                        <h3><i class="fas fa-camera"></i> Visit your photo album</h3>
+                        <h2><i class="fas fa-camera"></i> Visit your photo album</h2>
                         <img src={albums[0].pictures[0]}/>
                         </NavLink>
                     </div> 
                     : 
                     <h3><i class="fas fa-camera"></i> {title}</h3>}                 
             </div>          
-           
+           {albums.length === 0 && <AlbumCreate />}
         </div>
        
     )
